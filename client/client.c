c112e6a820cf45dfd5c17bbc493631650a3ab3a0
DS-000228 <dushuang0228@163.com>
2024-10-16 17:51:23 +0800

    实现基于 TCP 的客户端，通过控制台输入数据，并发送给服务器

--- client/client.c
+++ client/client.c
@@ -9,31 +9,30 @@
 int main() {
     int sock = 0;
     struct sockaddr_in serv_addr;
-    char *message = "Hello from Client";
+    char *message;
+    char buffer[1024] = {0};
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
+    // 创建 TCP 套接字
+    sock = socket(AF_INET, SOCK_STREAM, 0);
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-    
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
+    // 连接服务器
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
-    send(sock, message, strlen(message), 0);
-    char buffer[1024] = {0};
+    // 从控制台读取输入
+    printf("Enter message to send: ");
+    fgets(buffer, 1024, stdin);
+
+    // 发送数据给服务器
+    send(sock, buffer, strlen(buffer), 0);
+
+    // 接收服务器的回应
     read(sock, buffer, 1024);
-    printf("Response from server: %s\n", buffer);
-    
+    printf("Server: %s\n", buffer);
+
     close(sock);
     return 0;
 }
