14be3f8efb2f8eaaf0dad7be2eacba3adc5b1544
DS-000228 <dushuang0228@163.com>
2024-10-16 15:43:18 +0800

    2

--- ui/Dialog_ui.sh
+++ ui/Dialog_ui.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 while true; do
-    cmd=$(dialog --menu "Select an action" 15 50 3 \
-        1 "Insert Record" \
-        2 "Query Database" \
-        3 "Exit" 3>&1 1>&2 2>&3)
+    # 输入框让用户输入数据
+    INPUT=$(dialog --inputbox "Enter data to send to server:" 8 40 3>&1 1>&2 2>&3 3>&-)
+    
+    # 检查是否取消
+    if [ $? != 0 ]; then
+        break
+    fi
 
-    case $cmd in
-        1)
-            no=$(dialog --inputbox "Enter No:" 8 40 3>&1 1>&2 2>&3)
-            name=$(dialog --inputbox "Enter Name:" 8 40 3>&1 1>&2 2>&3)
-            score=$(dialog --inputbox "Enter Score:" 8 40 3>&1 1>&2 2>&3)
-            # Call the socket client here with $no, $name, $score
-            ;;
-        2)
-            # Call the socket client to query the database
-            ;;
-        3)
-            break
-            ;;
-    esac
+    # 调用客户端发送数据
+    echo "$INPUT" | ./client
+
+    # 查询数据库
+    dialog --msgbox "Querying database..." 5 40
+    sqlite3 server.db "SELECT * FROM data_table;" | dialog --textbox - 20 50
+
+    # 继续选项
+    dialog --yesno "Do you want to continue?" 6 25
+    if [ $? != 0 ]; then
+        break
+    fi
 done
 
